4ebd6b0e65e39292a625813e939d17de9af14e5c
Thomas van Latum <thomasvanlatum@Thomass-MacBook-Pro.local>
2019-04-23 09:46:50 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # ghost-gcs
 Ghost.org Google Cloud Storage adapter
+
+## Installation
+
+### Install from Git
+
+- Go into Ghost root directory
+- Navigate to the `/content/adapters/storage` directory
+- Download the adpater:
+
+```bash
+$ git clone https://github.com/thomas-vl/ghost-gcs.git
+```
+
+- Install dependencies:
+
+```bash
+$ cd ghost-gcs
+$ npm install
+```
+
+## Configuration
+"storage": {
+    "active": "ghost-gcs",
+    "ghost-gcs": {
+        "projectId": "projectid",
+        "key": "credentials.json",
+        "bucket": "bucketname",
+    }
+
---
+++ index.js
@@ -0,0 +1,82 @@
+const StorageBase = require('ghost-storage-base'),
+      {Storage} = require('@google-cloud/storage');
+//const common = require('../../../../current/core/server/lib/common');
+
+class GStore extends StorageBase {
+    constructor(config) {
+        super();
+        var storage = new Storage({"keyFilename": config.key});
+        this.bucket = storage.bucket(config.bucket)
+    }
+
+    exists(fileName, targetDir) {
+        return new Promise((resolve, reject) => {
+            this.bucket.file(targetDir+'/'+fileName).exists()
+            .then(data=>{
+                resolve(data[0])
+            })
+        })
+    }
+     
+    /**
+     * Saves the image to google cloud storage
+     * - image is the express image object
+     * - returns a promise which ultimately returns the full url to the uploaded image
+     *
+     * @param image
+     * @param targetDir
+     * @returns {*}
+     */
+    save(image, targetDir) {
+        return new Promise((resolve, reject) => {
+            targetDir = this.getTargetDir()
+            this.getUniqueFileName(image, targetDir).then(filename=>{
+                var opts = {
+                    predefinedAcl: 'publicRead',
+                    destination: filename
+                }
+                return this.bucket.upload(image.path, opts)
+            }).then(response =>{
+                var url = 'http://storage.googleapis.com/'+
+                    response[0].metadata.bucket+'/'+
+                    response[0].name
+                resolve(url)
+            }).catch(e=>{
+                //common.logging.error(JSON.stringify(e))
+                reject(e)
+            });
+        })
+  
+    }
+  
+    serve() {
+        return function(req, res, next) {
+            next();
+        };
+    }
+
+    delete(filename) {
+        //common.logging.info('delete:')
+        //common.logging.info(JSON.stringify(filename))
+        return this.bucket.file(filename).delete();
+    }
+  
+    read(filename) {
+        //common.logging.info('read:')
+        //common.logging.info('file name: '+filename)
+        var rs = this.bucket.file(filename).createReadStream(), contents = '';
+        return new Promise(function (resolve, reject) {
+            rs.on('error', function(err){
+                reject(err);
+            });
+            rs.on('data', function(data){
+            contents += data;
+            });
+            rs.on('end', function(){
+                resolve(content);
+            });
+        });
+    }
+}
+
+module.exports = GStore;
